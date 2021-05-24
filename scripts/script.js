4e73130da9bf3fcf91dd0dad37aeed2f8ff5979f
Utkarsh Mehta <utkarshmehta93@gmail.com>
2021-05-24 00:31:46 -0500

    bootstrap jquery make pause play as one button only

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $("#mycarousel").carousel({ interval: 3000 });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("span").hasClass("fa-pause")) {
+        $("#mycarousel").carousel("pause");
+        $('#carouselButton').children('span').removeClass("fa-pause");
+        $('#carouselButton').children('span').addClass("fa-play");
+    } else if($("#carouselButton").children("span").hasClass("fa-play")) {
+        $("#mycarousel").carousel("cycle");
+        $('#carouselButton').children('span').addClass("fa-pause");
+        $('#carouselButton').children('span').removeClass("fa-play");
+    }
+  });
+});
